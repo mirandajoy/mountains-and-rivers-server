3f0d0ab2d392140b8c1ff8288ca80f3e5f122ab6
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-08-15 15:03:38 -0300

    add websocket events using socket.io

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
-import cors from 'cors';
-import express from 'express';
-
-import 'dotenv/config';
-
-const app = express();
-const { PORT } = process.env;
-const port = PORT || 8080
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/test", (req, res) => {
-  res.send("hello world");
+const io = require("socket.io")(8080, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
 });
 
-app.listen(port, () => {
-  console.log(`This app is listening on port ${port}`);
+io.on("connection", (socket) => {
+  socket.on("gameDetails", (key, sessionValue) => {
+    io.emit("gameDetails", key, sessionValue);
+  });
+
+  socket.on("gameSetup", (setupDetails) => {
+    io.emit("gameSetup", setupDetails);
+  });
+
+  socket.on("boardPosition", (positionDetails) => {
+    io.emit("boardPosition", positionDetails);
+  });
+
+  socket.on("winner", (winner) => {
+    io.emit("winner", winner);
+  });
+
+  socket.on("activePlayer", (player) => {
+    io.emit("activePlayer", player);
+  });
+
+  socket.on("message", (message) => {
+    io.emit("message", message);
+  });
+
+  socket.on("diceRoll", (rollValues) => {
+    io.emit("diceRoll", rollValues);
+  });
 });
